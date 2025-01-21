e3821ac23de1fc6da1993d9e11d81f81294b8da6
abrown1wpi <andrewbrown2026@gmail.com>
2025-01-21 09:09:16 -0500

    commit

--- icsp/src/Workstation.jsx
+++ icsp/src/Workstation.jsx
@@ -1,14 +1,59 @@
-import React, { useRef, forwardRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 import map from './assets/mapRS.jpg'
+import pin from './assets/locatePin.png'
+
+const PinButton = ({onClick}) => {
+    return(
+        <img src={pin} alt='pin button' className='pinButton' onClick={onClick}></img>
+    )
+}
+
+const FollowPinObj = ({onClick}) => {
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+          setMousePosition({ x: e.clientX, y: e.clientY });
+        };
+    
+        window.addEventListener('mousemove', handleMouseMove);
+    
+        return () => {
+          window.removeEventListener('mousemove', handleMouseMove);
+        };
+      }, []); 
+      const componentStyle = {
+        position: 'absolute',
+        left: mousePosition.x,
+        top: mousePosition.y,
+        zIndex: 2,
+      };
+
+      const handleClick = () => {
+        onClick(mousePosition);
+      }
+    
+      return (
+          <div style={componentStyle} onClick={handleClick}>
+            <img src={pin} alt='pin button' className='pinFollow'></img>
+          </div>
+      );
+}
 
 const Workstation = () => {
-    
-    
+    const [followPins, setFollowPins] = useState([]);
+
+    const createFollowPin = () => {
+        setFollowPins([...followPins, <FollowPinObj key={followPins.length}></FollowPinObj>])
+    }
+
     return (
         <div className="mapRule">
-            <img src={map} alt="map" className="mapRule"/>
+            <img src={map} alt="map" className="imgMapRule"/>
+            <PinButton onClick={createFollowPin}></PinButton>
+            {followPins}
         </div>
+
     )
 }
 
